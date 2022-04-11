03b032a5614b81e5074c0df5932357e0d109d38e
neerav27 <102318321+neerav27@users.noreply.github.com>
2022-04-11 12:27:46 -0500

    Merging main to Neerav branch

--- anbm/src/pages/home/Home.jsx
+++ anbm/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Feed from '../../components/mainfeed/Feed';
+import NavBar from '../../components/NavBar/NavBar';
 import "./home.css"
 
 export default function Home() {
   return (
     <>
       <div className="homeContainer">
-          <Feed/>
+          <NavBar/>
+          
       </div>
     </>
   )
