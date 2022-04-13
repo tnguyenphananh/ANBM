22b70eb59467b31960b3c132afaab4ee81af597d
anchao0207 <atrieu@knox.edu>
2022-04-13 13:37:26 -0500

    nothing

---
+++ anbm/src/components/suggest/Suggest.jsx
@@ -0,0 +1,11 @@
+import "./suggest.css"
+
+export default function Suggest({users}) {
+  return (
+    <li 
+        className="rightsidebarFriend">
+        <img className="rightsidebarFriendImg" src="/assets/profile/template_3.jpg" alt=""/>
+        <span className="rightsidebarFriendName"> Jane Doe</span>
+    </li>
+  )
+}
