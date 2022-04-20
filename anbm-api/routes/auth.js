ace4ddf2a4a2511e9c1300caebfd590ae819084c
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-20 13:33:44 -0500

    auth

--- anbm-api/routes/auth.js
+++ anbm-api/routes/auth.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req,res)=>{
-    res.send("hey its auth route")
-})
+//REGISTER
+router.get("/register", async (req, res) => {
+    const user = await new User({
+        username: "john",
+        email: "john@gmail.com",
+        password: "123456"
+    })
 
-module.exports = router
+    await user.save();
+    res.send("ok")
+});
 
-
+module.exports = router;
