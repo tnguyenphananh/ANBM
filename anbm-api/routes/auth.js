33c949e5696f6497c8e2f00c01af6630c5085200
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-21 00:33:38 -0500

    fix

--- anbm-api/routes/auth.js
+++ anbm-api/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 router.post("/register", async (req, res) => {
     try{
          //generate new password
-        const salt = await bcrypt.getSalt(10);
+        const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
         //create new user
@@ -22,6 +22,21 @@ router.post("/register", async (req, res) => {
     } catch (err) {
         console.log(err);
     }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try{
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password");
+
+    res.stats(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
