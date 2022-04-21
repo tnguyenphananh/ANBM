ddc33b25684224da03f62bd6a510a064ef2e6d76
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-21 02:02:01 -0500

    s

--- anbm-api/routes/users.js
+++ anbm-api/routes/users.js
@@ -53,10 +53,10 @@ router.get("/:id", async (req, res) => {
 
 //follow a user
 router.put("/:id/follow", async (req, res) => {
-    if (req.body.userId !== req.params.id){
+    if (req.body.userId !== req.params.id) {
         try{
             const user = await User.findById(req.params.id);
-            const currentUser = await User.findById(req.params.userId);
+            const currentUser = await User.findById(req.body.userId);
             if(!user.followers.includes(req.body.userId)){
                 await user.updateOne({$push: { followers: req.body.userId } });
                 await currentUser.updateOne({$push: { followings: req.body.id } });
