2b8d68c9db1951db6fe2e48d69c4ba17c30a784a
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-21 01:06:01 -0500

    fix

--- anbm-api/models/User.js
+++ anbm-api/models/User.js
@@ -3,53 +3,57 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
     {
     username:{
-        type:String,
-        require:true,
-        min:3,
-        max:20,
-        unique:true
+        type: String,
+        require: true,
+        min: 3,
+        max: 20,
+        unique: true
     },
     email:{
-        type:String,
-        required:true,
-        max:50,
-        unique:true
+        type: String,
+        required: true,
+        max: 50,
+        unique: true
     },
     password:{
-        type:String,
-        required:true,
-        min:6
+        type: String,
+        required: true,
+        min: 6
     },
     profilePicture:{
-        type:String,
-        default:""
+        type: String,
+        default: ""
     },
     coverPicture:{
-        type:String,
-        default:""
+        type: String,
+        default: ""
     },
     followers:{
-        type:Array,
-        default:[]
+        type: Array,
+        default: []
     },
     followins:{
-        type:Array,
-        default:[]
+        type: Array,
+        default:[ ]
     },
     isAdmin:{
-        type:Boolean,
-        default:false
+        type: Boolean,
+        default: false
     },
     desc:{
-        type:String,
-        max:50
+        type: String,
+        max: 50,
+    },
+    city: {
+        type: String,
+        max: 50,
     },
     from:{
-        type:String,
-        max:50
+        type: String,
+        max: 50,
     },
     relationships:{
-        type:Number,
+        type: Number,
         enum: [1,2,3]
     }
 },
