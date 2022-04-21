cf83cba3ae71e568561d3702524b2c19c5570741
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-21 02:04:24 -0500

    e

--- anbm-api/routes/users.js
+++ anbm-api/routes/users.js
@@ -57,9 +57,9 @@ router.put("/:id/follow", async (req, res) => {
         try{
             const user = await User.findById(req.params.id);
             const currentUser = await User.findById(req.body.userId);
-            if(!user.followers.includes(req.body.userId)){
-                await user.updateOne({$push: { followers: req.body.userId } });
-                await currentUser.updateOne({$push: { followings: req.body.id } });
+            if(!user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $push: { followers: req.body.userId } });
+                await currentUser.updateOne({ $push: { followings: req.body.id } });
                 res.status(200).json("user has been followed");
             } else {
                 res.status(403).json("you already follow this user");
