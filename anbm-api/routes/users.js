1909266377cc924762310cb523fe264d00e1ffdf
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-21 01:22:43 -0500

    s

--- anbm-api/routes/users.js
+++ anbm-api/routes/users.js
@@ -30,7 +30,7 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     if (req.body.userId === req.params.id || req.body.isAdmin) { 
         try{
-            const user = await User.deleteOne(req.params.id);
+            await User.findByIdAndDelete(req.params.id);
             res.status(200).json("Account has been deleted");
         } catch (err){
             return res.status(500).json(err);
