6b536eb51b9ade95f7ee21a6ef1206632b2820bc
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-21 01:15:44 -0500

    fix

--- anbm-api/routes/users.js
+++ anbm-api/routes/users.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 //update user
 router.put("/:id", async (req, res) => {
     if (req.body.userId === req.params.id || req.user.isAdmin) { 
-        if(req.body.password){
+        if(req.body.password) {
             try{
                 const salt = await bcrypt.genSalt(10);
                 req.body.password = await bcrypt.hash(req.body.password, salt);
@@ -14,7 +14,7 @@ router.put("/:id", async (req, res) => {
             }
         }
         try{
-            const user = await User.findByIdAndUpdate(req.params, id, {
+            const user = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
             });
             res.status(200).json("Account has been updated");
