8f8c8efc62b6c187b8c5ed0d8669a5a7fac9b2b7
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-23 22:12:53 -0400

    e

---
+++ anbm-api/routes/AuthActions.js
@@ -0,0 +1,13 @@
+export const LoginStart = (userCredentials) => ({
+    type: "LOGIN_START",
+});
+
+export const LoginSuccess = (user) => ({
+    type: "LOGIN_START",
+    payload: user,
+});
+
+export const LoginFailure = (console.error();) => ({
+    type: "LOGIN_FAILURE",
+    payload: error
+});
