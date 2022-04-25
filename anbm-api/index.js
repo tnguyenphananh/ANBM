5bc0cca809a875a2820c4f2d2b27a9d35bf8d228
neerav27 <102318321+neerav27@users.noreply.github.com>
2022-04-25 13:36:17 -0500

    started file uploading

--- anbm-api/index.js
+++ anbm-api/index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
 
 dotenv.config();
 
@@ -21,6 +22,24 @@ mongoose.connect(process.env.MONGO_URL,
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+const storage = multer.diskStorage({
+    destination:(reg,file,cb)=>{
+        cb(null,"/anbm-api/public/images");
+    },
+    filename: (req,file,cb)=>{
+        cb(null,file.originalname);
+    },
+   
+});
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req,res) => {
+    try{
+return res.status(200).json("file uploaded successfully.");
+    }catch(err){
+        console.log(err);
+    }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
@@ -28,4 +47,4 @@ app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
     console.log("Backend sever is running!")
-})
+}) 
