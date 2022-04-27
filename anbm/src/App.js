81c6d3b9723cf5584340bf4939a43bbfe8abdfa8
bmwise <81723439+bmwise@users.noreply.github.com>
2022-04-26 22:34:17 -0500

    fix

--- anbm/src/App.js
+++ anbm/src/App.js
@@ -5,14 +5,15 @@ import Register from "./pages/home/register/Register";
 import Profile from "./pages/profile/profile";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {AuthContext} from "./context/AuthContext";
+import { Users } from "./dummyData";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={user ? <Home/> : <Register />}/>
-        <Route exact path="/login" element={user ? <Redirect to="/"/> : <Login/>}/>
-        <Route exact path="/register" element={user ? <Redirect to="/" /> : <Register/>}/>
+        <Route exact path="/" element={Users ? <Home/> : <Register />}/>
+        <Route exact path="/login" element={Users ? <Redirect to="/"/> : <Login/>}/>
+        <Route exact path="/register" element={Users ? <Redirect to="/" /> : <Register/>}/>
         <Route exact path="/profile" element={<Profile/>}/>
       </Routes>
     </Router>
