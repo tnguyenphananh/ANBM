93c88c5e7cb0b1f7713612fe5b0c9c0edc1a22f7
anchao0207 <atrieu@knox.edu>
2022-04-26 22:58:55 -0500

    fix in app.js, adding a misisng line of code and change redirect into navigate

--- anbm/src/App.js
+++ anbm/src/App.js
@@ -3,17 +3,18 @@ import Home from "./pages/home/home/Home";
 import Login from "./pages/home/login/Login";
 import Register from "./pages/home/register/Register";
 import Profile from "./pages/profile/profile";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
 import {AuthContext} from "./context/AuthContext";
 
 function App() {
+  const{user}=useContext(AuthContext);
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={user ? <Home/> : <Register />}/>
-        <Route exact path="/login" element={user ? <Redirect to="/"/> : <Login/>}/>
-        <Route exact path="/register" element={user ? <Redirect to="/" /> : <Register/>}/>
-        <Route exact path="/profile" element={<Profile/>}/>
+        <Route exact path="/login" element={user ? <Navigate to="/"/> : <Login/>}/>
+        <Route exact path="/register" element={user ? <Navigate to="/" /> : <Register/>}/>
+        <Route exact path="/profile/:username" element={<Profile/>}/>
       </Routes>
     </Router>
   );
