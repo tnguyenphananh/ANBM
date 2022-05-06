c18e2480541d5eecce99a7222a56dc3acc8a0c97
anchao0207 <atrieu@knox.edu>
2022-05-06 10:36:35 -0500

    band app

--- anbm-api/anbm/src/apiCalls.js
+++ anbm-api/anbm/src/apiCalls.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import { axiosInstance } from "./config";
 
 export const loginCall = async (userCredentials, dispatch) => {
     dispatch ({ type: "LOGIN_START"});
     try{
-        const res = await axios.post("auth/login", userCredentials);
+        const res = await axiosInstance.post("auth/login", userCredentials);
         dispatch ({ type: "LOGIN_SUCCESS", payload: res.data});
     } catch (err){
         dispatch ({ type: "LOGIN_FAILURE", payload: err});
--- anbm-api/index.js
+++ anbm-api/index.js
@@ -47,6 +47,12 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
+app.use(express.static(path.join(__dirname,"/anbm/build")));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/anbm/build','index.html'));
+});
+
+app.listen(process.env.PORT || 8800, () => {
     console.log("Backend sever is running!")
 }) 
