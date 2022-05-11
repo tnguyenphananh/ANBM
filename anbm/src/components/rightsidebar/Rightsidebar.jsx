100793587cb3f76092ecdcddbc2993a74742a152
bmwise <81723439+bmwise@users.noreply.github.com>
2022-05-11 12:33:42 -0500

    revert back to the meeting

--- anbm/src/components/rightsidebar/Rightsidebar.jsx
+++ anbm/src/components/rightsidebar/Rightsidebar.jsx
@@ -6,49 +6,10 @@ import { TrendingUp } from "@material-ui/icons";
 import { Whatshot } from "@material-ui/icons";
 import { Users } from "../../dummyData";
 import Suggest from "../suggest/Suggest";
-import Profile from "../../pages/profile/profile";
-import { useContext, useEffect } from "react";
-import {Add} from "@material-ui/icons";
-import { useState } from "react";
-import  {axios}  from "axios";
-import { AuthContext } from "../../context/AuthContext";
-import { Link } from "react-router-dom";
 
 export default function Rightsidebar({ user }) {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
-    useEffect(()=>{
-        setFollowed(currentUser.followings.includes(user?.id))
-        
-    },[currentUser, user?.id]);
-
-    useEffect(()=>{
-        const getFriends = async () => {
-            try{
-                const friendList = await axios.get("/users/friends/" +user._id);
-                setFriends(friendList.data);
-            }catch(err){
-                console.log(err);
-            }
-        }; 
-        getFriends();
-     }, [user?._id]);
-      
-
-    const handleClick = async ()=> {
-        try{
-            if(followed){
-                await axios.put("/users/" + user._id+"/unfollow", {userId:currentUser._id});
-                dispatch({type:"UNFOLLOW",payload:user._id})
-            }else{
-                await axios.put("/users/" + user._id+"/follow", {userId:currentUser._id});
-                dispatch({type:"FOLLOW",payload:user._id});
-            }
-        }catch(err){
-            console.log(err)
-        }
-        setFollowed(!followed)
-    };
     const HomeRightbar = () => {
         return (
             <>
@@ -113,17 +74,30 @@ export default function Rightsidebar({ user }) {
                 </div>
                 <h4 className="rightsidebarTitle">Top Songs</h4>
                 <div className="rightsidebarFollowings">
-                    {friends.map(friend=>(
-                        <Link to={"/profile/"+friend.username} style={{textDecoration:"none"}}>
                     <div className="rightsidebarFollowing">
-                        <img src={friend.profilePicture ? PF+friend.profilePicture
-                        :PF+"profile/noAvatar.png"} 
-                        alt="" className="rightsidebarFollowingImg" />
-                        <span className="rightsidebarFollowingName"> {friend.username}</span>
+                        <img src={`${PF}profile/photo1.jpg`} alt="" className="rightsidebarFollowingImg" />
+                        <span className="rightsidebarFollowingName"> Tipitipitiu</span>
                     </div>
-                    </Link>
-                    ))} 
-                    
+                    <div className="rightsidebarFollowing">
+                        <img src={`${PF}profile/photo2.jpg`} alt="" className="rightsidebarFollowingImg" />
+                        <span className="rightsidebarFollowingName">John Cena</span>
+                    </div>
+                    <div className="rightsidebarFollowing">
+                        <img src={`${PF}profile/photo3.jpg`} alt="" className="rightsidebarFollowingImg" />
+                        <span className="rightsidebarFollowingName">John Cena</span>
+                    </div>
+                    <div className="rightsidebarFollowing">
+                        <img src={`${PF}profile/photo1.jpg`} alt="" className="rightsidebarFollowingImg" />
+                        <span className="rightsidebarFollowingName">John Cena</span>
+                    </div>
+                    <div className="rightsidebarFollowing">
+                        <img src={`${PF}profile/photo2.jpg`} alt="" className="rightsidebarFollowingImg" />
+                        <span className="rightsidebarFollowingName">John Cena</span>
+                    </div>
+                    <div className="rightsidebarFollowing">
+                        <img src={`${PF}profile/photo3.jpg`} alt="" className="rightsidebarFollowingImg" />
+                        <span className="rightsidebarFollowingName">John Cena</span>
+                    </div>
                 </div>
             </>
 
--- anbm/src/components/share/Share.jsx
+++ anbm/src/components/share/Share.jsx
@@ -1,67 +1,24 @@
 import "./share.css"
 import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons"
-import { AuthContext } from "../../context/AuthContext";
-import { useContext } from "react";
-import { useRef } from "react";
-import { useState } from "react";
-import axios from "axios";
 
-export default function () {
-  const{user} = useContext(AuthContext);
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-  const desc = useRef();
-  const [file,setFile] = useState(null)
-
-  const submitHandler = async (e) => {
-    e.preventDefault()
-    const newPost = {
-      userId: user._id,
-      desc: desc.current.value
-    }
-    if(file){
-      const data = new FormData();
-      const fileName = Date.now() + file.name;
-      data.append("file", file);
-      data.append("name", fileName);
-      newPost.img = fileName;
-      try{
-        await axios.post("/upload", data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    try{
-      await axios.post("/post", newPost);
-      window.location.reload();
-    } catch (err) {}
-  };
-
+export default function Share() {
   return (
     <div className="share">
       <div className="shareWrapper">
         <div className="shareTop">
-          <img className="shareProfileImg" src= {user.profilePicture ? PF + user.profilePicture : PF + "noAvatar.png"}  alt="" />
+          <img className="shareProfileImg" src="/assets/profile/template_3.jpg" alt="" />
           <input
-            placeholder= {"what's in your mind " + user.username + "?"}
+            placeholder="what's in your mind Safak?"
             className="shareInput"
-            ref={desc}
           />
         </div>
         <hr className="shareHr" />
-        <form className="shareBottom" onSubmit={submitHandler}>
+        <div className="shareBottom">
           <div className="shareOptions">
-            <label htmlFor="file" className="shareOption">
+            <div className="shareOption">
               <PermMedia htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
-              <input 
-              style= {{display: "none"}} 
-              type= "file" 
-              id= "file" 
-              accept= ".png,.jpeg,.jpg" 
-              onChange={(e)=>setFile(e.target.files[0])} 
-              />
-            </label>
+            </div>
             <div className="shareOption">
               <Label htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
@@ -75,8 +32,8 @@ export default function () {
               <span className="shareOptionText">Feeling</span>
             </div>
           </div>
-          <button className="shareButton" type="submit">Share</button>
-        </form>
+          <button className="shareButton">Share</button>
+        </div>
       </div>
     </div>
   )
